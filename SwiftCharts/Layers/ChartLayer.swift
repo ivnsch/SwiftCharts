2507f2176292a809ae9d2adebca068a38fbe2da2
ischuetz <ivanschuetz@gmail.com>
2015-05-22 19:34:59 +0200

    Reword comment

--- SwiftCharts/Layers/ChartLayer.swift
+++ SwiftCharts/Layers/ChartLayer.swift
@@ -16,6 +16,6 @@ public protocol ChartLayer {
     // Use this to draw directly in chart's context.
     // Don't do anything processor intensive here - this is executed as part of draw(rect) thus has to be quick.
     // Note that everything drawn here will appear behind subviews added by any layer (regardless of position in layers array)
-    // Anything done here can also be done adding a subview in chartInialized and drawing on that. The reason this method exists is only performance - as long as we know the layers will appear always behind (e.g. axis lines, guidelines) there's no reason to bloat the view hierarchy.
+    // Everything done here can also be done adding a subview in chartInialized and drawing on that. The reason this method exists is only performance - as long as we know the layers will appear always behind (e.g. axis lines, guidelines) there's no reason to create new views.
     func chartViewDrawing(#context: CGContextRef, chart: Chart)
 }
