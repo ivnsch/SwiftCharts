8c8d553b6094392cf70a3b548da53a593e6c17cc
Benedikt Iltisberger <benedikt.iltisberger@rewe-digital.com>
2015-08-08 18:24:34 +0200

    Dynamic framework created for carthage.

---
+++ Examples/SwiftCharts/SwiftCharts.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCharts.h
+//  SwiftCharts
+//
+//  Created by Benedikt Iltisberger on 08.08.15.
+//  Copyright (c) 2015 ivanschuetz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCharts.
+FOUNDATION_EXPORT double SwiftChartsVersionNumber;
+
+//! Project version string for SwiftCharts.
+FOUNDATION_EXPORT const unsigned char SwiftChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCharts/PublicHeader.h>
+
+
