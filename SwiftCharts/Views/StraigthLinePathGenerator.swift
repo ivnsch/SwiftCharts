adf81e0ff3b323196859b0c5bbd90d4283b4c667
Colin Reisterer <creisterer@projectdecibel.com>
2015-08-13 13:04:35 -0400

    Only iterate through array if at least 2 points in points array
    Previously this would generate a runtime error if no points were in array.
    If 1 point is in array, it won't error but I think its more explicit this way.

--- SwiftCharts/Views/StraigthLinePathGenerator.swift
+++ SwiftCharts/Views/StraigthLinePathGenerator.swift
@@ -12,20 +12,24 @@ class StraigthLinePathGenerator: ChartLinesViewPathGenerator {
     func generatePath(points points: [CGPoint], lineWidth: CGFloat) -> UIBezierPath {
 
         let progressline = UIBezierPath()
-        progressline.lineWidth = lineWidth
-        progressline.lineCapStyle = .Round
-        progressline.lineJoinStyle = .Round
+      
+        if points.count >= 2 {
+
+            progressline.lineWidth = lineWidth
+            progressline.lineCapStyle = .Round
+            progressline.lineJoinStyle = .Round
         
-        for i in 0..<(points.count - 1) {
-            let p1 = points[i]
-            let p2 = points[i + 1]
+            for i in 0..<(points.count - 1) {
+                let p1 = points[i]
+                let p2 = points[i + 1]
             
-            progressline.moveToPoint(p1)
-            progressline.addLineToPoint(p2)
+                progressline.moveToPoint(p1)
+                progressline.addLineToPoint(p2)
+            }
+
+            progressline.closePath()
         }
-        
-        progressline.closePath()
-        
+
         return progressline
     }
 }
