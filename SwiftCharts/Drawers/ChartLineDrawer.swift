03b81e814a71c8b3824847c29ebf84512bfd002d
Pair Station <pairstation@01385-imac.local>
2015-12-10 10:22:25 -0800

    Dynamic axis stroke width
    Use the axis setting's stroke width to set the axis stroke width

--- SwiftCharts/Drawers/ChartLineDrawer.swift
+++ SwiftCharts/Drawers/ChartLineDrawer.swift
@@ -12,14 +12,16 @@ class ChartLineDrawer: ChartContextDrawer {
     private let p1: CGPoint
     private let p2: CGPoint
     private let color: UIColor
+    private let strokeWidth: CGFloat
     
-    init(p1: CGPoint, p2: CGPoint, color: UIColor) {
+    init(p1: CGPoint, p2: CGPoint, color: UIColor, strokeWidth: CGFloat = 0.2) {
         self.p1 = p1
         self.p2 = p2
         self.color = color
+        self.strokeWidth = strokeWidth
     }
-    
+
     override func draw(context context: CGContextRef, chart: Chart) {
-        ChartDrawLine(context: context, p1: self.p1, p2: self.p2, width: 0.2, color: self.color)
+        ChartDrawLine(context: context, p1: self.p1, p2: self.p2, width: self.strokeWidth, color: self.color)
     }
 }
