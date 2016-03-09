12d358bfd5403330f4c993fbd9dc18347228a66c
Iain Bryson <iain@iain-bryson.ca>
2016-03-09 10:07:43 +0300

    Fixes for pull request feedback.

--- Examples/Examples/PanScatterExample.swift
+++ Examples/Examples/PanScatterExample.swift
@@ -21,6 +21,42 @@ private enum Shape {
     case Triangle, Square, Circle, Cross
 }
 
+public class SimpleAxisLabelView: UIView {
+    
+    weak var chart: Chart?
+    var axisLabelDrawer: ChartLabelDrawer?
+    
+    public init(frame: CGRect, text: String, settings: ChartLabelSettings, chart: Chart) {
+        super.init(frame: frame)
+        self.sharedInit()
+
+        self.chart = chart // ugly
+        
+        let axisLabel = ChartAxisLabel(text: text, settings: settings)
+        let labelSize = ChartUtils.textSize(axisLabel.text, font: settings.font)
+        let settings = settings
+        let newSettings = ChartLabelSettings(font: settings.font, fontColor: settings.fontColor, rotation: settings.rotation, rotationKeep: settings.rotationKeep)
+        self.axisLabelDrawer = ChartLabelDrawer(text: axisLabel.text, screenLoc: CGPointMake(
+            frame.origin.x + (frame.size.width / 2) + labelSize.height,
+            (frame.size.height - 0.0 - labelSize.height)), settings: newSettings)
+        
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.sharedInit()
+    }
+    
+    func sharedInit() {
+        self.backgroundColor = UIColor.clearColor()
+    }
+    
+    override public func drawRect(rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        axisLabelDrawer!.draw(context: context!, chart: chart!)
+    }
+}
+
 class PanScatterExample: UIViewController, UIGestureRecognizerDelegate {
     
     private var charts = [String:Chart]()
@@ -53,7 +89,14 @@ class PanScatterExample: UIViewController, UIGestureRecognizerDelegate {
             .Type3 : (.Cross, UIColor.blackColor())
         ]
 
-        let visibleChartFrame = ExamplesDefaults.chartFrame(self.view.bounds)
+        let axisLabelHeight = CGFloat(20.0)
+        
+        // Use the whole superview, but carve out some space for the axis labels
+        let visibleChartFrame = CGRectMake(self.view.bounds.origin.x + axisLabelHeight,
+                                                self.view.bounds.origin.y,
+                                                self.view.bounds.size.width - axisLabelHeight,
+                                                self.view.bounds.size.height - axisLabelHeight)
+        
         let underlyingChartFrame = CGRectMake((visibleChartFrame.origin.x - visibleChartFrame.size.width),
                                               (visibleChartFrame.origin.y - visibleChartFrame.size.height),
                                               3.0 * visibleChartFrame.size.width,
@@ -62,10 +105,10 @@ class PanScatterExample: UIViewController, UIGestureRecognizerDelegate {
         let xValues = 0.stride(through: 450, by: 50).map {ChartAxisValueInt($0, labelSettings: labelSettings)}
         let yValues = 0.stride(through: 300, by: 50).map {ChartAxisValueInt($0, labelSettings: labelSettings)}
         
-        let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "X Axis title", settings: labelSettings))
-        let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "Y Axis title", settings: labelSettings.defaultVertical()))
+        let xModel = ChartAxisModel(axisValues: xValues)
+        let yModel = ChartAxisModel(axisValues: yValues)
         
-        // TODO: This default CoordsSapce up being very wastefu when we split out the elements into their own views.  Because the various elements (x axis, y axis and plot area) are positioned by this helper relative to a larger underlying canvas, by default we'd need to allocate UIViews of the size of the underlying frame for each of them, tripling our memory consumption.
+        // TODO: This default CoordsSapce up being very wasteful when we split out the elements into their own views.  Because the various elements (x axis, y axis and plot area) are positioned by this helper relative to a larger underlying canvas, by default we'd need to allocate UIViews of the size of the underlying frame for each of them, tripling our memory consumption.
         // This problem can be avoided for the y axis since it "hugs" the left border (and we an create a tall, narrow UIView without needing to allocate a whole extra chart-sized one), and is not a huge deal for the plot area since the only space we need to waste is the offset where the y axis lives -- it already is mostly the size of the underlying frame.  But it's a big problem for the x axis as we end up having to allocate an UIView the entire size of the underlying frame so that it can render at the bottom of this frame.
         let underlyingCoordSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: ExamplesDefaults.chartSettings, chartFrame: underlyingChartFrame, xModel: xModel, yModel: yModel)
         let (xAxis, yAxis, _) = (underlyingCoordSpace.xAxis, underlyingCoordSpace.yAxis, underlyingCoordSpace.chartInnerFrame)
@@ -88,22 +131,23 @@ class PanScatterExample: UIViewController, UIGestureRecognizerDelegate {
         )
         let plotAreaUIView = UIView(frame: CGRect(origin: CGPoint(x: visibleCoordSpace.chartInnerFrame.origin.x + visibleChartFrame.origin.x, y: (visibleCoordSpace.chartInnerFrame.origin.y+visibleChartFrame.origin.y)), size: visibleCoordSpace.chartInnerFrame.size))
         plotAreaUIView.clipsToBounds = true
-        plotAreaUIView.backgroundColor = UIColor(colorLiteralRed: 0.9, green: 0.5, blue: 0.9, alpha: 0.05) // debug colors
+        plotAreaUIView.backgroundColor = UIColor(red: 0.9, green: 0.5, blue: 0.9, alpha: 0.05) // debug colors
         plotAreaUIView.addSubview(plotChart.view)
         self.view.addSubview(plotAreaUIView)
 
         
         // take the axes from the full-size chart and create a new chart with it so it gets it's own view.
+
         let xAxisChart = Chart(frame: CGRect(origin: CGPointMake(underlyingChartFrame.origin.x + innerFrame.origin.x, underlyingChartFrame.origin.y + innerFrame.size.height*0.0), size: CGSize(width: underlyingChartFrame.size.width, height: underlyingChartFrame.size.height) ), layers: [underlyingCoordSpace.xAxis])
-        xAxisChart.view.backgroundColor = UIColor(colorLiteralRed: 0.9, green: 0.5, blue: 0.9, alpha: 0.05) // debug colors
+        xAxisChart.view.backgroundColor = UIColor(red: 0.9, green: 0.5, blue: 0.9, alpha: 0.05) // debug colors
         xAxisChart.view.transform = CGAffineTransformTranslate(CGAffineTransformIdentity, 0.0, -1*visibleChartFrame.size.height)
         
-        let yAxisChart = Chart(frame: CGRect(origin: CGPointMake(underlyingChartFrame.origin.x - ExamplesDefaults.chartSettings.axisTitleLabelsToLabelsSpacing, underlyingChartFrame.origin.y), size: CGSize(width: underlyingCoordSpace.chartInnerFrame.origin.x, height: underlyingChartFrame.size.height)), layers: [underlyingCoordSpace.yAxis])
-        yAxisChart.view.backgroundColor = UIColor(colorLiteralRed: 0.9, green: 0.9, blue: 0.5, alpha: 0.05) // debug colors
+        let yAxisChart = Chart(frame: CGRect(origin: CGPointMake(underlyingChartFrame.origin.x - visibleChartFrame.origin.x, underlyingChartFrame.origin.y), size: CGSize(width: underlyingCoordSpace.chartInnerFrame.origin.x, height: underlyingChartFrame.size.height)), layers: [underlyingCoordSpace.yAxis])
+        yAxisChart.view.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.5, alpha: 0.05) // debug colors
         yAxisChart.view.transform = CGAffineTransformTranslate(CGAffineTransformIdentity, +visibleChartFrame.size.width, 0.0)
         
         // view to clip out the bottom left rectangular region where the axes would tend to overlap each other
-        let axisClipperSubview = UIView(frame: CGRect(origin: CGPoint(x: visibleChartFrame.origin.x, y: (visibleCoordSpace.chartInnerFrame.origin.y+visibleChartFrame.origin.y)), size: CGSizeMake(innerFrame.origin.x, innerFrame.size.height)))
+        let axisClipperSubview = UIView(frame: CGRect(origin: CGPoint(x: visibleChartFrame.origin.x, y: (visibleCoordSpace.chartInnerFrame.origin.y + visibleChartFrame.origin.y)), size: CGSizeMake(innerFrame.origin.x, innerFrame.size.height)))
         axisClipperSubview.clipsToBounds = true;
         
         // we can chose to clip either x or y:
@@ -117,6 +161,12 @@ class PanScatterExample: UIViewController, UIGestureRecognizerDelegate {
         self.view.addSubview(xAxisChart.view)
 
         self.view.addSubview(axisClipperSubview)
+        
+        let xAxisView = SimpleAxisLabelView(frame: visibleChartFrame, text: "X Axis title", settings: labelSettings, chart: plotChart)
+        let yAxisView = SimpleAxisLabelView(frame: visibleChartFrame, text: "Y Axis title", settings: labelSettings.defaultVertical(), chart: plotChart)
+
+        self.view.addSubview(xAxisView)
+        self.view.addSubview(yAxisView)
 
         self.charts = ["x" : xAxisChart, "plot" : plotChart, "y" : yAxisChart]
     }
