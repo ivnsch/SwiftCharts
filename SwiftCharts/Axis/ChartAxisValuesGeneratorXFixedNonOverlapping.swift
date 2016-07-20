4c6db314e9882ac00d57fa3a71dccf8cb74dd426
ischuetz <ivanschuetz@gmail.com>
2016-07-20 09:41:56 +0200

    Add zoom handling to fixed x label generator

--- SwiftCharts/Axis/ChartAxisValuesGeneratorXFixedNonOverlapping.swift
+++ SwiftCharts/Axis/ChartAxisValuesGeneratorXFixedNonOverlapping.swift
@@ -10,58 +10,69 @@ import UIKit
 
 public class ChartAxisValuesGeneratorXFixedNonOverlapping: ChartAxisValuesGeneratorFixed {
 
-    private let labelFont: UIFont
-    private let axisValues: [ChartAxisValue]
+    public let labelFont: UIFont
+    public let axisValues: [ChartAxisValue]
+    
+    public let maxLabelSize: CGSize
+    public let totalLabelSize: CGSize
+    public let spacing: CGFloat = 4
     
     public init(axisValues: [ChartAxisValue], labelFont: UIFont) {
         self.axisValues = axisValues
         self.labelFont = labelFont
+        
+        (totalLabelSize, maxLabelSize) = ChartAxisValuesGeneratorXFixedNonOverlapping.calculateLabelsDimensions(axisValues, labelFont: labelFont)
+        
         super.init(values: axisValues.map{$0.scalar})
     }
     
     public override func axisLayerInitialized(layer: ChartAxisLayer) {
-        let nonOverlappingValues = ChartAxisValuesGeneratorXFixedNonOverlapping.selectNonOverlappingXAxisLabels(axisValues, screenLength: layer.frame.width, labelFont: labelFont)
-        values = nonOverlappingValues.map{$0.scalar}
+        updateAxisValues(layer.axis)
     }
     
-    private static func selectNonOverlappingXAxisLabels(xAxisLabels: [ChartAxisValue], screenLength: CGFloat, labelFont: UIFont) -> [ChartAxisValue] {
+    public override func generate(axis: ChartAxis) -> [Double] {
+        updateAxisValues(axis)
+        return super.generate(axis)
+    }
+    
+    private func updateAxisValues(axis: ChartAxis) {
+        values = selectNonOverlappingXAxisLabels(axisValues, screenLength: axis.screenLength, labelFont: labelFont).map{$0.scalar}
+    }
+    
+    private func selectNonOverlappingXAxisLabels(xAxisLabels: [ChartAxisValue], screenLength: CGFloat, labelFont: UIFont) -> [ChartAxisValue] {
         
         // Select only the x-axis labels which would prevent any overlap
         let spacePerXTick = screenLength / CGFloat(xAxisLabels.count)
         
         var relaxedXAxisValues: [ChartAxisValue] = []
         
-        let largestXLabel = ChartAxisValuesGeneratorXFixedNonOverlapping.getLargestLabels(xAxisLabels, labelFont: labelFont)
-        
         var x: CGFloat = 0, currentLabelEnd: CGFloat = 0
-        xAxisLabels.forEach({(axisValue) in
+        xAxisLabels.forEach({axisValue in
             x += spacePerXTick
             if (currentLabelEnd <= x) {
                 relaxedXAxisValues.append(axisValue)
-                currentLabelEnd = x + largestXLabel.width + 4 // 2px border
+                currentLabelEnd = x + maxLabelSize.width + spacing
             }
         })
         
-        // Always show the last label
-        if (relaxedXAxisValues.last! != xAxisLabels.last!) {
-            relaxedXAxisValues[relaxedXAxisValues.count - 1] = xAxisLabels.last!
+        if let relatedLast = relaxedXAxisValues.last, axisLabelLast = relaxedXAxisValues.last {
+            // Always show the last label
+            if (relatedLast != axisLabelLast) {
+                relaxedXAxisValues[relaxedXAxisValues.count - 1] = xAxisLabels.last!
+            }
         }
         
         return relaxedXAxisValues
     }
     
-    private static func getLargestLabels(values: [ChartAxisValue], labelFont: UIFont) -> CGSize {
-        let maxSize = CGSize(width: CGFloat.max, height:  CGFloat.max)
-        
-        let maxLabelWidth = values.map({
-            $0.labels[0].text.boundingRectWithSize(maxSize,
-                options: NSStringDrawingOptions.UsesLineFragmentOrigin,
-                attributes: [NSFontAttributeName:  labelFont],
-                context: nil).size
-        }).reduce(CGSizeZero, combine: { (lhs: CGSize, rhs: CGSize) -> CGSize in
-            return CGSize(width: max(lhs.width, rhs.width), height: max(lhs.height, rhs.height))
+    private static func calculateLabelsDimensions(values: [ChartAxisValue], labelFont: UIFont) -> (total: CGSize, max: CGSize) {
+        return values.map({
+            ChartUtils.textSize($0.labels[0].text, font: labelFont)
+        }).reduce((total: CGSizeZero, max: CGSizeZero), combine: {(lhs: (total: CGSize, max: CGSize), rhs: CGSize) in
+            return (
+                CGSize(width: lhs.total.width + rhs.width, height: lhs.total.height + rhs.height),
+                CGSize(width: max(lhs.max.width, rhs.width), height: max(lhs.max.height, rhs.height))
+            )
         })
-        
-        return maxLabelWidth
     }
 }
