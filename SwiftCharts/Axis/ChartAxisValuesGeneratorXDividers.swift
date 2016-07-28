9e54bde13c7f14376f88bfb295a1c4799328184e
ischuetz <ivanschuetz@gmail.com>
2016-07-28 14:44:08 +0200

    Add 'nice' bounds to axis value calculation

--- SwiftCharts/Axis/ChartAxisValuesGeneratorXDividers.swift
+++ SwiftCharts/Axis/ChartAxisValuesGeneratorXDividers.swift
@@ -21,16 +21,17 @@ public class ChartAxisValuesGeneratorXDividers: ChartAxisGeneratorMultiplier {
     private var minValue: Int
     private var maxValue: Int
     private let minSpace: CGFloat
-    private let tryRound: Bool
+    private let nice: Bool
     private let preferredDividers: Int
     
     private let maxTextSize: CGFloat
-
-    public init(minValue: Int, maxValue: Int, preferredDividers: Int, tryRound: Bool, formatter: NSNumberFormatter, font: UIFont, minSpace: CGFloat = 10) {
+    
+    public init(minValue: Int, maxValue: Int, preferredDividers: Int, nice: Bool, formatter: NSNumberFormatter, font: UIFont, minSpace: CGFloat = 10) {
+        
         self.minValue = minValue
         self.maxValue = maxValue
         
-        self.tryRound = tryRound
+        self.nice = nice
         self.preferredDividers = preferredDividers
         self.minSpace = minSpace
         
@@ -60,47 +61,24 @@ public class ChartAxisValuesGeneratorXDividers: ChartAxisGeneratorMultiplier {
     
     public func calculateFittingRangeAndFactor(axis: ChartAxis) -> (min: Int, max: Int, factor: Double)? {
         
-        let roundA = Double(minValue) / 100
-        let myround = Int(pow(10, round(log10(roundA))))
-        var growDelta = 1
-        if abs(minValue) > myround && abs(maxValue) > myround {
-            if abs(minValue) > myround {
-                minValue = minValue.floorMultiple(myround)
-            }
-            if abs(maxValue) > myround {
-                maxValue = maxValue.ceilMultiple(myround)
-            }
-            growDelta = myround
+        if nice {
+            let niceMinMax = self.nice(minValue: Double(minValue), maxValue: Double(maxValue))
+            self.minValue = Int(niceMinMax.minValue)
+            self.maxValue = Int(niceMinMax.maxValue)
         }
         
-        var length = maxValue - minValue
+        let growDelta = Int(pow(10, round(log10(Double(min(abs(minValue), abs(maxValue)))))))
+        
+        let length = maxValue - minValue
         let maxDelta = length / 6 // try to expand range to the left/right of initial range
         
-        func findFittingFactor(modelLength: Int) -> Int? {
-            
-            let factors = modelLength.primeFactors
-            
-            var lastFitting: Int?
-            
-            // look for max factor which fits
-            for factor in factors {
-                if minSpace + ((maxTextSize + minSpace) * CGFloat(factor)) < axis.screenLength {
-                    lastFitting = factor
-                } else { // when labels don't fit, return the previous fitting
-                    return lastFitting
-                }
-            }
-            return lastFitting
-        }
-
         var fittingFactorsPerDelta = [(min: Int, max: Int, factor: Double)]()
         var currentDelta = 0
         var counter = 0 // just in case
         while(currentDelta < maxDelta && counter < 100) {
             let min = minValue - currentDelta
             let max = maxValue + currentDelta
-            
-            if let fittingFactor = findFittingFactor(max - min) {
+            if let fittingFactor = findFittingFactor(axis, modelLength: max - min) {
                 fittingFactorsPerDelta.append((min, max, Double(fittingFactor)))
             }
 
@@ -123,6 +101,29 @@ public class ChartAxisValuesGeneratorXDividers: ChartAxisGeneratorMultiplier {
         return maxFittingFactorOpt
     }
     
+    // inspired by d3 nice
+    private func nice(minValue minValue: Double, maxValue: Double) -> (minValue: Double, maxValue: Double) {
+        let span = pow(10, round(log(maxValue - minValue)/log(10)) - 1)
+        return(minValue: (floor(minValue / span) * span), maxValue: (ceil(maxValue / span) * span))
+    }
+    
+    private func findFittingFactor(axis: ChartAxis, modelLength: Int) -> Int? {
+        
+        let factors = modelLength.primeFactors
+        
+        var lastFitting: Int?
+        
+        // look for max factor which fits
+        for factor in factors {
+            if minSpace + ((maxTextSize + minSpace) * CGFloat(factor)) < axis.screenLength {
+                lastFitting = factor
+            } else { // when labels don't fit, return the previous fitting
+                return lastFitting
+            }
+        }
+        return lastFitting
+    }
+    
     public override func axisInitialized(axis: ChartAxis) {
         
         func defaultInit() {
@@ -131,7 +132,7 @@ public class ChartAxisValuesGeneratorXDividers: ChartAxisGeneratorMultiplier {
             self.multiplier = axis.length / Double(dividers)
         }
         
-        if tryRound {
+        if nice {
             if let rangeAndMultiplier = calculateFittingRangeAndFactor(axis) {
                 self.minValue = rangeAndMultiplier.min
                 self.maxValue = rangeAndMultiplier.max
