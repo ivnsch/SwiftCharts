a830e6792d4b4a2ab566724ddaff957507d80536
ischuetz <ivanschuetz@gmail.com>
2016-08-18 14:17:05 +0200

    Add separate field for original label text

--- SwiftCharts/Axis/ChartAxisLabel.swift
+++ SwiftCharts/Axis/ChartAxisLabel.swift
@@ -11,9 +11,13 @@ import UIKit
 /// A model of an axis label
 public class ChartAxisLabel {
 
+    /// Displayed text. Can be truncated.
     public let text: String
+    
     public let settings: ChartLabelSettings
 
+    public private(set) var originalText: String
+    
     var hidden: Bool = false
 
     /// The size of the bounding rectangle for the axis label, taking into account the font and rotation it will be drawn with
@@ -29,13 +33,15 @@ public class ChartAxisLabel {
     public init(text: String, settings: ChartLabelSettings) {
         self.text = text
         self.settings = settings
+        self.originalText = text
     }
     
-    func copy(text: String? = nil, settings: ChartLabelSettings? = nil, hidden: Bool? = nil) -> ChartAxisLabel {
+    func copy(text text: String? = nil, settings: ChartLabelSettings? = nil, originalText: String? = nil, hidden: Bool? = nil) -> ChartAxisLabel {
         let label = ChartAxisLabel(
             text: text ?? self.text,
             settings: settings ?? self.settings
         )
+        self.originalText = originalText ?? self.originalText
         self.hidden = hidden ?? self.hidden
         return label
     }
