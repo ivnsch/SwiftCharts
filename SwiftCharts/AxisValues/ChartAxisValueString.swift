0f3dc78c0d384cf719bb6bc921e1e69aabdbe5e7
ischuetz <ivanschuetz@gmail.com>
2016-08-30 17:39:42 +0200

    Make field public

--- SwiftCharts/AxisValues/ChartAxisValueString.swift
+++ SwiftCharts/AxisValues/ChartAxisValueString.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public class ChartAxisValueString: ChartAxisValue {
    
-    let string: String
+    public let string: String
     
     public init(_ string: String = "", order: Int, labelSettings: ChartLabelSettings = ChartLabelSettings()) {
         self.string = string
