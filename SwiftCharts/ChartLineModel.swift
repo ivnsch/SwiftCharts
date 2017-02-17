23cc1d3909634478666571fc641eb7a7674a6bb1
Ivan Schuetz <ivanschuetz@gmail.com>
2017-02-17 20:39:07 +0100

    Make properties public

--- SwiftCharts/ChartLineModel.swift
+++ SwiftCharts/ChartLineModel.swift
@@ -12,26 +12,26 @@ import UIKit
 public struct ChartLineModel<T: ChartPoint> {
 
     /// The array of chart points that the line should be drawn with. In a simple case this would be drawn as straight line segments connecting each point.
-    let chartPoints: [T]
+    public let chartPoints: [T]
 
     /// The color that the line is drawn with
-    let lineColor: UIColor
+    public let lineColor: UIColor
 
     /// The width of the line in points
-    let lineWidth: CGFloat
+    public let lineWidth: CGFloat
     
-    let lineJoin: LineJoin
+    public let lineJoin: LineJoin
     
-    let lineCap: LineCap
+    public let lineCap: LineCap
     
     /// The duration in seconds of the animation that is run when the line appears
-    let animDuration: Float
+    public let animDuration: Float
 
     /// The delay in seconds before the animation runs
-    let animDelay: Float
+    public let animDelay: Float
     
     /// The dash pattern for the line
-    let dashPattern: [Double]?
+    public let dashPattern: [Double]?
     
     public init(chartPoints: [T], lineColor: UIColor, lineWidth: CGFloat = 1, lineJoin: LineJoin = .round, lineCap: LineCap = .round, animDuration: Float, animDelay: Float, dashPattern: [Double]? = nil) {
         self.chartPoints = chartPoints
